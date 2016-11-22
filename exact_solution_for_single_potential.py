17a18a6c429554149fea341bb64be0bc5886f53d
erik <erik@innomis.com>
2016-11-22 10:09:24 +0100

    farfield approx init

--- exact_solution_for_single_potential.py
+++ exact_solution_for_single_potential.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.special import gamma
+
 from scipy import integrate
 from scipy.special import jv , yv
 import matplotlib.pyplot as plot
@@ -51,12 +53,12 @@ my_imag_function = np.frompyfunc(my_imag,1,1)
 my_real_function = np.frompyfunc(my_real,1,1)
 
 
-nu=1.0/2.0;
-m=0;
-k=np.sqrt(2.0+0.00*complex(0,1.0));
-a=0.5;
-b=a+10.0;
-a_t = np.arange(a, b, 0.001)
+nu=2.0/2.0;
+m=1.0;
+k=np.sqrt(1.0+0.00*complex(0,1.0));
+a=1.1;
+b=a+5.52;
+a_t = np.arange(a, b, 0.01)
 asol = integrate.odeint(solvr, [ex_sol(a), ex_sol_diff(a)], a_t);
 sol = [0]*len(asol);
 sol_s = [0]*len(asol); 
@@ -90,9 +92,52 @@ fig2 = plot.plot(a_t,one);
 fig2 = plot.plot(a_t,minus_one);
 fig2 = plot.plot(a_t,null_line);
 
-#plot.figure(5);
-#fig2 = plot.plot(a_t,my_real_function(-a_t*ys*bj(m,nr*a_t)+nr*a_t*y*bdiff(bj,m,nr*a_t,1))+my_real_function(a_t*ys*bh(m,nr*a_t)-nr*a_t*y*bdiff(bh,m,nr*a_t,1)),'r+');
-#fig2 = plot.plot(a_t,my_imag_function(-a_t*ys*bj(m,nr*a_t)+nr*a_t*y*bdiff(bj,m,nr*a_t,1)),'y+');
+plot.figure(5);
+
+# Near field # nu = 1;  k = m = 1
+#a=0.1;b=a+14.52;nu=1.0;k=1.0;m=1.0;step = 0.01 # nu = 1;  k = m = 1
+# Far field # nu = 1;  k = m = 1
+#a=25.1;b=a+0.52;nu=1.0;k=1.0;m=1.0;step = 0.00001  # nu = 1;  k = m = 1
+
+# Near field # nu = 2;  k = m = 1
+#a=0.1;b=a+4.52;nu=2.0;k=1.0;m=1.0;step = 0.01 # nu = 2;  k = m = 1
+# Far field # nu = 2;  k = m = 1
+#a=25.1;b=a+0.052;nu=2.0;k=1.0;m=1.0;step = 0.00001  # nu = 2;  k = m = 1
+
+# Near field # nu = 3;  k = m = 1
+#a=0.1;b=a+4.52;nu=3.0;k=1.0;m=1.0;step = 0.01 # nu = 3;  k = m = 1
+#Far field # # nu = 3;  k = m = 1
+a=25.1;b=a+0.00052;nu=3.0;k=1.0;m=1.0;step = 0.00001  # nu = 3;  k = m = 1
+
+
+
+a_t = np.arange(a, b, step)
+y = ex_sol(a_t);
+ys = ex_sol_diff(a_t);
+one = [1]*len(a_t);
+minus_one = [-1]*len(a_t); 
+null_line =  [0]*len(a_t);
+nr = index_sol(a_t);
+
+# Far field # nu = 1;  k = m = 1
+#far_field = -1.25*bj(m,1.0/(nu+1)*k*nr*a_t)*np.power(a_t,1.0) # nu = 1;  k = m = 1
+
+# Far field # nu = 2;  k = m = 1
+#far_field = -1.775*bj(m,1.99995/(nu+1)*k*nr*a_t)*np.power(a_t,1.5); ## nu = 2;  k = m = 1
+
+#Far field # # nu = 3;  k = m = 1
+far_field = -11.00*bj(m,2.999806/(nu+1)*k*nr*a_t)*np.power(a_t,1.5); # nu = 3;  k = m = 1
+
+
+
+plot.figure(5);
+fig2 = plot.plot(a_t,my_real_function(-a_t*ys*bj(m,nr*a_t)+nr*a_t*y*bdiff(bj,m,nr*a_t,1)),'r--');
+fig2 = plot.plot(a_t,far_field,'b--');
+fig2 = plot.plot(a_t,null_line);
+
+plot.figure(6);
+fig2 = plot.plot(a_t,my_real_function(-a_t*ys*bj(m,nr*a_t)+nr*a_t*y*bdiff(bj,m,nr*a_t,1)-far_field),'r--');
+fig2 = plot.plot(a_t,null_line);
 
 
 plot.show();
